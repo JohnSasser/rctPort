d24ff35de7da18b6210e34e15e2ad32aadf9b4fa
John Sasser <cw.john.sasser@gmail.com>
2022-01-16 01:14:57 -0500

    opacity animation to hamburger dropdown, translate was not working.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,26 +3,23 @@ import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 
 import './style.css';
+import { CSSTransition } from 'react-transition-group';
 
 const Navbar = props => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(true);
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   const updateNavbar = () => {
-    if (props.windowWidth === 'desktop') {
+    if (props.windowWidth > 800) {
       setNavbarOpen(true);
     } else setNavbarOpen(false);
-  };
-
-  const handleHamburgerClick = () => {
-    hamburgerOpen === true ? setHamburgerOpen(false) : setHamburgerOpen(true);
   };
 
   useEffect(() => {
     updateNavbar();
   }, [props.windowWidth]);
 
-  console.log(`hamburgerOpen: ${hamburgerOpen} || navbarOpen : ${navbarOpen}`);
+  // console.log(`hamburgerOpen: ${hamburgerOpen} || navbarOpen : ${navbarOpen}`);
 
   return navbarOpen === true ? (
     <div id="navbar">
@@ -60,63 +57,68 @@ const Navbar = props => {
       <div className="links-2"></div>
     </div>
   ) : (
-    <>
-      {' '}
-      <div className="hamburger-menu-container">
-        <div id="hamburger-icon-button" onClick={handleHamburgerClick}>
-          <FaBars />
-        </div>
-        {hamburgerOpen ? (
-          <div id="dropdown-menu">
-            <li>
-              <Link
-                onClick={handleHamburgerClick}
-                id="about-me-link"
-                to="/about-me"
-              >
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleHamburgerClick}
-                id="portfolio-link"
-                to="/portfolio"
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleHamburgerClick}
-                id="homepage-link"
-                to="/reactPortfolio"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleHamburgerClick}
-                id="contact-me-link"
-                to="/contact-me"
-              >
-                Contact Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleHamburgerClick}
-                id="resume-link"
-                to="/resume"
-              >
-                Resume´
-              </Link>
-            </li>
-          </div>
-        ) : null}
+    <div className="hamburger-menu-container">
+      <div
+        id="hamburger-icon-button"
+        onClick={() => setHamburgerOpen(!hamburgerOpen)}
+      >
+        <FaBars />
       </div>
-    </>
+      <CSSTransition
+        in={hamburgerOpen}
+        timeout={500}
+        classNames="hamburger-dropdown"
+        unmountOnExit
+      >
+        <div id="dropdown-menu">
+          <li>
+            <Link
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+              id="about-me-link"
+              to="/about-me"
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+              id="portfolio-link"
+              to="/portfolio"
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+              id="homepage-link"
+              to="/reactPortfolio"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+              id="contact-me-link"
+              to="/contact-me"
+            >
+              Contact Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+              id="resume-link"
+              to="/resume"
+            >
+              Resume´
+            </Link>
+          </li>
+        </div>
+      </CSSTransition>
+    </div>
   );
 };
 
