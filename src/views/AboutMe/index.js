be151cc6cb98c628cd37708d646ea1413b65250b
John Sasser <cw.john.sasser@gmail.com>
2022-05-14 12:59:22 -0400

    about me text adjustment

--- src/views/AboutMe/index.js
+++ src/views/AboutMe/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../../components/Footer';
 import { frontEndSVGs, backEndSVGs, toolsSVGs } from '../../utils/vectorJSX.js';
+import {content1} from '../AboutMe/content.js';
+import './style.css';
 
 // import { Container, Row, Col } from 'react-bootstrap';
+// console.log(content.content1);
 
-import './style.css';
 
 const AboutMe = () => {
   const headshot = process.env.PUBLIC_URL + '/images/bike-race-headshot.jpeg';
@@ -30,23 +32,7 @@ const AboutMe = () => {
           <img id="headshot-image" src={`${headshot}`} alt="headshot" />
         </div>
         <div className="about-section-text">
-          <p>
-            I am a highly motivated Software Developer with an unrivaled passion
-            for creating stunning and technically elegant web based solutions.
-            Front-end preference with back-end/full stack experience and the
-            competency to work collaboratively on a vast array of projects. I am
-            happiest working on a team to deliver solutions synergistically. Not
-            only do I take great pride in my ability to see things from the end
-            user's perspective; I'm also a driven learner with the ability to
-            see the big picture, compartmentalize tasks, and methodically tackle
-            the project at hand. When a tough opportunity arises, I am able to
-            pivot and seek the resources needed to analyze, attain, and
-            implement solutions. I am fixated on continuous learning,
-            adaptation, and innovation. I manage all aspects of web development,
-            from concept to design, development, launch, maintenance, and user
-            support. I enjoy the client-facing role and working closely with
-            team members to produce high-quality deliverables.
-          </p>
+          {content1}
         </div>
       </div>
       <div id="technologies-icons-parent-div">
